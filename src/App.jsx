4e07080b3ab3cf6f0ffd0c84c48f7b5f8aaf42ca
tuffii <100721921+tuffii@users.noreply.github.com>
2024-05-21 18:51:47 +0300

    [feat]: added draft video calling sustem (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import { ReactKeycloakProvider } from '@react-keycloak/web'
-import Keycloak from 'keycloak-js'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './page/Home/Home'
-import Error from './page/Error/Error'
-import { userContactApi } from './components/misc/UserContactApi.jsx'
-import PrivateRoute from './components/misc/PrivateRoute.jsx'
-import Navbar from './components/misc/Navbar'
-import { Dimmer, Header, Icon } from 'semantic-ui-react'
-import { config } from './Constants'
+import React from "react";
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import Keycloak from 'keycloak-js';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './page/Home/Home';
+import Error from './page/Error/Error';
+import PrivateRoute from './components/misc/PrivateRoute';
+import Navbar from './components/misc/Navbar';
+import { Dimmer, Header, Icon } from 'semantic-ui-react';
+import { config } from './Constants';
 
-function App() {
+
+import CallPage from "./page/VideoCallPage/CallPage";
+
+const App = () => {
   const keycloak = new Keycloak({
     url: `${config.url.KEYCLOAK_BASE_URL}`,
     realm: 'dzoms-realm',
     clientId: 'dzoms-ui-service-client',
-  })
-  const initOptions = { pkceMethod: 'S256' }
+  });
+  const initOptions = { pkceMethod: 'S256' };
 
   const handleOnEvent = async (event, error) => {
     if (event === 'onAuthSuccess') {
       if (keycloak.authenticated) {
+        // Дополнительная логика аутентификации
       }
     }
-  }
+  };
 
   const loadingComponent = (
     <Dimmer inverted active={true} page>
@@ -34,21 +38,27 @@ function App() {
         </Header.Content>
       </Header>
     </Dimmer>
-  )
+  );
+
+  // Функция для создания собрания
+  const createMeeting = async () => {
+    // Здесь должна быть логика для создания идентификатора собрания
+    return "12345"; // Пример: возвращаем фиксированный ID собрания
+  };
 
   return (
     <ReactKeycloakProvider authClient={keycloak} initOptions={initOptions} LoadingComponent={loadingComponent} onEvent={(event, error) => handleOnEvent(event, error)}>
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/home' element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route path='/' element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route path="/call/:meetingId" element={<CallPage />} />
           <Route path='/error' element={<Error />} />
           <Route path="*" element={<Navigate to="/error" />} />
         </Routes>
       </Router>
     </ReactKeycloakProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/joinCall/joinCall.jsx
+++ src/components/joinCall/joinCall.jsx
@@ -1,11 +1,35 @@
-import './joinCall.css'
+// components/JoinCall.jsx
+import React, { useState } from "react";
+import SettingsMeeting from "../../components/SettingsMeeting/SettingsMeeting";
+import "./joinCall.css";
 
-export default function joinCall() {
+const JoinCall = () => {
+  const [meetingId, setMeetingId] = useState("");
+  const [isJoining, setIsJoining] = useState(false);
+
+  const handleJoinMeeting = async () => {
+    setIsJoining(true);
+    return meetingId;
+  };
+
   return (
-    <div class='join-call'>
-      <span>присоединиться к звонку</span>
-      <input type='text' placeholder='код доступа' />
-      <button>присоединиться</button>
-    </div>
-  )
-}
+    <>
+      {!isJoining ? (
+        <div className="join-call">
+          <span>Присоединиться к звонку</span>
+          <input
+            type="text"
+            value={meetingId}
+            onChange={(e) => setMeetingId(e.target.value)}
+            placeholder="код доступа"
+          />
+          <button onClick={() => setIsJoining(true)}>Присоединиться</button>
+        </div>
+      ) : (
+        <SettingsMeeting onCreateMeeting={handleJoinMeeting} />
+      )}
+    </>
+  );
+};
+
+export default JoinCall;
